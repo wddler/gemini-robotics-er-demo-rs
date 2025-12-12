9740e1a3afb8b3043d378e12043c3093acdc9512
Denis Zatyagov <wddler@gmail.com>
2025-12-12 15:46:02 +0100

    add text edit field and send button

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use std::fs;
 use std::path::Path;
 
@@ -6,17 +6,28 @@ use std::path::Path;
 async fn index() -> impl Responder {
     // Note: The path to index.html is relative to the Cargo.toml of the project.
     // This assumes you are running the web server from the root of your Rust project.
-    let index_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("../gemini-robotics-er-demo-rs/index.html");
+    let index_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("index.html");
     match fs::read_to_string(index_path) {
         Ok(body) => HttpResponse::Ok().content_type("text/html").body(body),
         Err(_) => HttpResponse::NotFound().body("404 Not Found: index.html not found"),
     }
 }
 
+#[post("/send")]
+async fn send(body: web::Bytes) -> impl Responder {
+    match std::str::from_utf8(&body) {
+        Ok(s) => {
+            println!("Received from client: {}", s);
+            HttpResponse::Ok().body(format!("Server received: {}", s))
+        }
+        Err(_) => HttpResponse::BadRequest().body("Invalid request body"),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Starting server at http://127.0.0.1:8080");
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(index).service(send))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
